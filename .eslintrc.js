0622114d536130818de6ca5c19478b1157ac9d81
Git Automerge <jcguarinpenaranda@gmail.com>
2017-11-19 14:25:55 -0500

    removed some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   "extends": "airbnb",
   rules: {
     'class-methods-use-this': 0,
+    'jsx-a11y/interactive-supports-focus': 0,
+    'react/no-array-index-key': 1
   },
   env: {
     jest: true,
