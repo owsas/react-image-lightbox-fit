4b2494c92a2b76d9ce17d2205ca428664dc8c738
Git Automerge <jcguarinpenaranda@gmail.com>
2017-11-19 14:28:19 -0500

    Created component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,105 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Lightbox from 'react-image-lightbox';
 
-export default () => (
-  <div>Hello world!</div>
-);
+export default class ReactImageLightboxFit extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false,
+      photoIndex: 0,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    const toggled = !this.state.isModalOpen;
+    this.setState({ isModalOpen: toggled });
+  }
+
+  render() {
+    const { images } = this.props;
+    const { photoIndex } = this.state;
+
+    return (
+      <div>
+
+        <div
+          className="img"
+          role="button"
+          onClick={this.toggleModal}
+          onKeyPress={this.handleKeyDown}
+          style={{
+            height: this.props.height,
+            backgroundColor: 'black',
+            textAlign: 'center',
+            width: '100%',
+            backgroundImage: `url(${images[photoIndex]})`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain',
+          }}
+        />
+
+        <div style={{ padding: 10 }}>
+          {images.map((img, index) => {
+            const onImgClick = () => {
+              this.setState({ photoIndex: index });
+              this.toggleModal();
+            };
+
+            return (
+              <div
+                className="relatedImg"
+                key={`img-${index}`}
+                onClick={onImgClick}
+                role="button"
+                onKeyPress={this.handleKeyDown}
+                style={{
+                backgroundImage: `url(${img})`,
+                backgroundSize: 'cover',
+                backgroundColor: 'black',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                display: 'inline-block',
+                width: 60,
+                height: 60,
+                marginRight: 5,
+                marginBottom: 5,
+              }}
+              />
+            );
+          })}
+        </div>
+
+
+        {this.state.isModalOpen && <Lightbox
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + (images.length - 1)) % images.length]}
+
+          onCloseRequest={() => this.setState({ isModalOpen: false })}
+          onMovePrevRequest={() => this.setState({
+            photoIndex: (photoIndex + (images.length - 1)) % images.length,
+          })}
+          onMoveNextRequest={() => this.setState({
+            photoIndex: (photoIndex + 1) % images.length,
+          })}
+        />}
+
+      </div>
+    );
+  }
+}
+
+
+ReactImageLightboxFit.defaultProps = {
+};
+
+
+ReactImageLightboxFit.propTypes = {
+  height: PropTypes.number.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
--- test/index.test.jsx
+++ test/index.test.jsx
@@ -3,18 +3,89 @@ import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import Test from '../src/index';
 
-describe('Test', () => {
-  test('should be a component', () => {
-    expect(<Test />).toBeTruthy();
+test('should be a component', () => {
+  expect(<Test images={['test']} height={200} />).toBeTruthy();
+});
+
+describe('initial state', () => {
+  test('isModalOpen should be false', () => {
+    const c = new Test({});
+    expect(c.state.isModalOpen).toEqual(false);
   });
 
-  test('should say "Hello, world!"', () => {
-    const s = shallow(<Test />);
-    expect(s.text()).toEqual('Hello world!');
+  test('photoIndex should be 0', () => {
+    const c = new Test({});
+    expect(c.state.photoIndex).toEqual(0);
+  });
+});
+
+describe('#toggleModal', () => {
+  test('should toggle state.isModalOpen', () => {
+    const s = shallow(<Test images={['test']} height={200} />);
+    s.instance().toggleModal();
+    expect(s.instance().state.isModalOpen).toBe(true);
+
+    s.instance().toggleModal();
+    expect(s.instance().state.isModalOpen).toBe(false);
+  });
+});
+
+describe('Rendered view', () => {
+  test('should have the image to be shown', () => {
+    const s = shallow(<Test images={['test']} height={200} />);
+
+    // test image exists
+    expect(s.find('.img').exists()).toBe(true);
+
+    // test background image is shown
+    const style = s.find('.img').prop('style');
+    expect(style.backgroundImage).toEqual('url(test)');
   });
 
-  test('should match snapshot', () => {
-    const s = shallow(<Test />);
+  test('should display all the other images', () => {
+    const s = shallow(<Test images={['test', 'test2', 'test3']} height={200} />);
+
+    // test images exist
+    expect(s.find('.relatedImg').length).toBe(3);
+  });
+
+  test('on click on the main .img, should toggle modal', () => {
+    const s = shallow(<Test images={['test', 'test2', 'test3']} height={200} />);
+
+    // verify modal is closed
+    expect(s.state().isModalOpen).toBe(false);
+
+
+    // click the image
+    s.find('.img').simulate('click');
+
+    // verify modal is opened
+    expect(s.state().isModalOpen).toBe(true);
+  });
+
+
+  test('on click on a .relatedImg, should toggle modal', () => {
+    const s = shallow(<Test images={['test', 'test2', 'test3']} height={200} />);
+
+    // verify modal is closed
+    expect(s.state().isModalOpen).toBe(false);
+
+
+    // click the image
+    s.find('.relatedImg').at(0).simulate('click');
+
+    // verify modal is opened
+    expect(s.state().isModalOpen).toBe(true);
+  });
+
+  test('should match snapshot when isModalOpen is false', () => {
+    const s = shallow(<Test images={['test', 'test2']} height={200} />);
+    expect(toJSON(s)).toMatchSnapshot();
+  });
+
+  test('should match snapshot when isModalOpen is false', () => {
+    const s = shallow(<Test images={['test', 'test2']} height={200} />);
+    s.instance().toggleModal();
     expect(toJSON(s)).toMatchSnapshot();
   });
 });
